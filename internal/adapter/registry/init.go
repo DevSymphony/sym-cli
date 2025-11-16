048770d6d04ae40e5942abf56e0a9c97fe12fcd2
ikjeong <jipark7937@gmail.com>
2025-11-16 02:20:41 +0000

    feat: register java adapters in registry
    - Create default adapter registry initialization
    - Register all adapters: ESLint, Prettier, TSC, Checkstyle, PMD

---
+++ internal/adapter/registry/init.go
@@ -0,0 +1,44 @@
+package registry
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/DevSymphony/sym-cli/internal/adapter/checkstyle"
+	"github.com/DevSymphony/sym-cli/internal/adapter/eslint"
+	"github.com/DevSymphony/sym-cli/internal/adapter/pmd"
+	"github.com/DevSymphony/sym-cli/internal/adapter/prettier"
+	"github.com/DevSymphony/sym-cli/internal/adapter/tsc"
+)
+
+// DefaultRegistry creates and populates a registry with all available adapters.
+func DefaultRegistry() *Registry {
+	reg := NewRegistry()
+
+	// Determine tools directory
+	toolsDir := getToolsDir()
+	workDir := getWorkDir()
+
+	// Register JavaScript/TypeScript adapters
+	reg.Register(eslint.NewAdapter(toolsDir, workDir))
+	reg.Register(prettier.NewAdapter(toolsDir, workDir))
+	reg.Register(tsc.NewAdapter(toolsDir, workDir))
+
+	// Register Java adapters
+	reg.Register(checkstyle.NewAdapter(toolsDir, workDir))
+	reg.Register(pmd.NewAdapter(toolsDir, workDir))
+
+	return reg
+}
+
+// getToolsDir returns the default tools directory.
+func getToolsDir() string {
+	home, _ := os.UserHomeDir()
+	return filepath.Join(home, ".sym", "tools")
+}
+
+// getWorkDir returns the current working directory.
+func getWorkDir() string {
+	wd, _ := os.Getwd()
+	return wd
+}
